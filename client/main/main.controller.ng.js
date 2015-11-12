df16554ba361604735f4c458b4e5b1ea39cb8f85
Renzo Rombolà <renzo.rombola@mm-one.com>
2015-11-12 09:06:42 +0100

    Minor updates

--- client/main/main.controller.ng.js
+++ client/main/main.controller.ng.js
@@ -98,5 +98,6 @@ angular.module('kataApp')
                 }
             });
         });
-	
+
+
 		});
--- server/images.collection.ng.js
+++
@@ -1,5 +0,0 @@
-//definizione di FsCollection per gestire le immagini con upload dalla dropzone
-
-MyImages = new FS.Collection('myImages', {
-    stores: [new FS.Store.GridFS("myImages")]
-});
